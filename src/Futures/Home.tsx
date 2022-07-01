d5d5fcd300763e94168a7aed68fa81e6b966ffaa
MIK <marwa.brj@gmail.com>
2022-07-01 12:47:16 +0200

    home page

--- src/Futures/Home.tsx
+++ src/Futures/Home.tsx
@@ -1,18 +1,52 @@
-import React from "react";
-import {StyleSheet, View, Text} from "react-native";
+import React, { useState,useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {StyleSheet, View, Text,TouchableOpacity} from "react-native";
+import AccessPage from "./AccessPage";
 
 export default function Home({ navigation }: { navigation: any }) {
+  const [author, setAuthor] = useState("");
+  const [isDionnected, setIsDisconnected]= useState(false)
+  const getData = async () => {
+    const jsonValue = await AsyncStorage.getItem("author");
+    setAuthor(jsonValue ?? " YOU");
+  };
+
+  useEffect(() => {
+    getData();
+    setAuthor(author)
+  }, [isDionnected]);
+
+  const disconnect = async () => {
+    await AsyncStorage.removeItem("author");
+    setIsDisconnected(true);
+    return setAuthor("")
+    }
+
   return (
-    <View style={styles.container}>
-      <Text>Voici la home Page</Text>
-            {/* <Text
+  <>
+    {
+        isDionnected ?
+        <AccessPage setIsDisconnected={setIsDisconnected}></AccessPage>
+          :
+      <View style={styles.container}>
+      <Text style={styles.text}>Hello {author}</Text>
+      <Text>Need to share some notes ?</Text>
+      <Text
         onPress={() => {
-          navigation.navigate("Detail");
+          navigation.navigate("New");
         }}
       >
-        Aller sur la page Detail
-      </Text> */}
-    </View>
+        Let's Start!
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={disconnect}>
+        <Text style={{fontWeight:'bold', }}>
+        Disconnect
+        </Text>
+      </TouchableOpacity>
+          </View>  
+    }
+      </>
+    
   );
 }
 
@@ -20,8 +54,19 @@ export default function Home({ navigation }: { navigation: any }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ebebeb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#101010',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  button: {
+    padding: 20,
+    marginTop: 100,
+    borderRadius: 20,
+    backgroundColor: "#D1ACA5"
+  }
 });
